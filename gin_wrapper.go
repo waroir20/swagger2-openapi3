f489a58435ed0c0632260d76ae16accd94d8ad54
Sean Doyle <Sean.Doyle@ey.com>
2025-03-04 16:51:38 -0600

    working gin

--- gin_wrapper.go
+++ gin_wrapper.go
@@ -1,16 +1,20 @@
 package swagger2openapi3
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/gin-gonic/gin"
 	"github.com/swaggest/swgui"
 	"github.com/swaggest/swgui/v5emb"
 )
 
 func AddRoute(router *gin.Engine, path, pathToSpec, title string) {
-	router.StaticFile(path, pathToSpec)
+	split := strings.SplitN(pathToSpec, "/", 3)
+	router.Static(path, fmt.Sprintf("%s/%s", split[0], split[1]))
 	router.GET(path, gin.WrapH(v5emb.NewHandlerWithConfig(swgui.Config{
 		Title:            title,
-		SwaggerJSON:      pathToSpec,
+		SwaggerJSON:      fmt.Sprintf("%s/%s", path, split[2]),
 		BasePath:         "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.17.14/",
 		InternalBasePath: path,
 		ShowTopBar:       true,
